560ead7e7f1da9a11918e6500ef901fb8a2d6b5c
Felipe Barreto <felipebarreto148@gmail.com>
2022-10-13 19:43:15 -0300

    configuration vuepress

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  title: 'Base Components',
+  description: 'Está é uma biblioteca Vue Js.',
+  base: '/base-components/',
+  themeConfig: {
+    sidebar: [
+      {
+        title: 'Components',
+        collapsable: false,
+        children: [
+          '/components/input-text.md',
+          '/components/input-textarea.md',
+        ],
+      },
+    ],
+  },
+};
---
+++ docs/README.md
@@ -0,0 +1,4 @@
+# Minha biblioteca de componentes Aqui está uma breve introdução.
+
+## Instalação
+``` $ npm install @barretof20/base-components```
